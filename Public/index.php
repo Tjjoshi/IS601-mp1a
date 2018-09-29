59e8cb55029f5bb66c0491d5ea13d9d06fb7836f
toral <tj244@njit.edu>
2018-09-29 14:49:11 -0400

    Task:add method

--- Public/index.php
+++ Public/index.php
@@ -5,11 +5,11 @@
  * Date: 9/25/18
  * Time: 9:34 PM
  */
- $test= "myfile"
+
 class main{
     static public function start(){
-        echo $test;
+        echo "test";
     }
 }
-
+main :: start();
 
