ce69ecb350d1b2c2aed7f12e7e11284e966a26a3
toral <tj244@njit.edu>
2018-10-06 18:04:13 -0400

    feature: added bootstrap link

--- Public/index.php
+++ Public/index.php
@@ -11,7 +11,7 @@ class main{
 
     static public function start($filename){
         $records = csv::getRecords($filename);
-       $table =html::createTable($records);
+        $table =html::createTable($records);
         system::printPage($table);
 
     }
@@ -75,6 +75,16 @@ class html{
 
    public static function createTable($array)
 {
+    echo "<html lang=\"en\">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset=\"utf-8\">
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+  <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">
+  <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>
+  <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>
+</head>
+</html>";
         // start table
         $html = '<table>';
         // header row
